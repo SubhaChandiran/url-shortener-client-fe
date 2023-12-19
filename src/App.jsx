f5d42d39b56eaf1a66e94016deba832db52ff947
SubhaChandiran <jerrysubash@gmail.com>
2023-12-19 23:29:07 +0530

    before changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const shortenUrl = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/shorten', {
+        originalUrl,
+      });
+
+      setShortUrl(response.data.shortUrl);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>URL Shortener</h1>
+      <input
+        type="text"
+        placeholder="Enter the URL to shorten"
+        value={originalUrl}
+        onChange={(e) => setOriginalUrl(e.target.value)}
+      />
+      <button onClick={shortenUrl}>Shorten URL</button>
+      {shortUrl && (
+        <div>
+          <p>Shortened URL:</p>
+          <a href={`http://localhost:5000/${shortUrl}`} target="_blank" rel="noopener noreferrer">
+            {`http://localhost:5000/${shortUrl}`}
+          </a>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
